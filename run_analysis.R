fcf02e6ad91820185b859eefb277b4520e591b7e
Timothy Johnstone <timothy.g.johnstone@gmail.com>
2015-04-12 21:16:59 -0400

    cleaned up and reordered code, the datasets are now merged at the beginning before modifying labels. Creates a complete data frame (still need to fix labels) and is ready for creation of tidy frame.

--- run_analysis.R
+++ run_analysis.R
@@ -2,7 +2,8 @@
 ## Timothy Johnstone, April 2015
 ## Analysis script for the final project of Coursera Data Science Series, 
 ## Course 3: Getting and Cleaning Data 'getdata-013'
-## For more information, see the attached README.md and CODEBOOK.md
+
+## For more information, PLEASE see the attached README.md and CODEBOOK.md
 
 ##THINGS TO REMOVE BEFORE FINAL PUB
 setwd("~/Dropbox/Coursera/3_Getting_and_cleaning_data/Projects/UCI HAR Dataset/")
@@ -14,40 +15,65 @@ require(tidyr)
 
 ##########
 
-# Make a data frame to translate activity numbers into activity names
+# Load in test dataset files
+testx <- read.table("test/X_test.txt")
+testy <-  read.table("test/y_test.txt")$V1
+testsubject <- read.table("test/subject_test.txt")$V1
+# Load in training dataset files
+trainx <- read.table("train/X_train.txt")
+trainy <-  read.table("train/y_train.txt")$V1
+trainsubject <- read.table("train/subject_train.txt")$V1
+
+#merge test and training set
+bothx <- rbind(testx, trainx)
+bothy <- c(testy, trainy)
+bothsubject <- c(testsubject, trainsubject)
+
+
+# Uses descriptive activity names to name the activities in the data set
 activitylabels <- read.table('activity_labels.txt', sep="\t", stringsAsFactors = F)
 activitylabels <- separate(activitylabels, V1, c("num","label"), " ")
+activitylabels$num <- as.numeric(activitylabels$num)
 activitylabels$label <- tolower(activitylabels$label)
 activitylabels$label <- sub("_","",activitylabels$label)
 
 # Make a data frame to translate feature numbers into feature names
 featurelabels <- read.table('features.txt', sep="\t", stringsAsFactors = F)
 featurelabels <- separate(featurelabels, V1, c("num","label"), " ")
-# Separate out just the features we care about - See note in README!
+featurelabels$num <- as.numeric(featurelabels$num)
+# Separates out just the features we care about - See note in README!
 # The (commented out) version will not include separate mean()-x, mean()-y, and mean()-z vars
-# FIX THIS FIX THIS FIX THIS FIX THIS FIX THIS to include mean and sd, properly escape parentheses in grep statement
-featurelabelsMeanStd <- featurelabels[c(grep('.mean()', featurelabels$label, fixed=T),grep('.std()', featurelabels$label, fixed=T)),]
 # featurelabelsmeanstd <- featurelabels[grep(".mean\\(\\)$|.std\\(\\)$", featurelabels$label),]
+selectedfeaturelabels <- featurelabels[grep('.mean\\(\\)|.std\\(\\)', featurelabels$label),]  
+  
+# Relabel and factorize tasks
+bothy <- activitylabels$label[match(bothy, activitylabels$num,)]
+bothy <- factor(bothy,levels = activitylabels$label)
 
-# Load in test dataset files
-testx <- read.table("test/X_test.txt")
-testy <-  read.table("test/y_test.txt")$V1
-testsubject <- read.table("test/subject_test.txt")$V1
+# Factorize subjects
+bothsubject <- as.factor(bothsubject)
 
-# Select and label the actual observations
+# Add column which indicates whether this subject is in the test or training set
+bothset <- factor(c(rep("test", length(testsubject)), rep("training", length(trainsubject))), levels=c("test","training"))
+
+# Extracts only the measurements on the mean and standard deviation for each measurement. 
+bothx <- bothx[,selectedfeaturelabels$num]
+names(bothx) <- selectedfeaturelabels$label
+# Appropriately labels the data set with descriptive variable names. 
+names(bothx) <- tolower(names(bothx))
+names(bothx) <- sub('\\(\\)', '', names(bothx))
+names(bothx) <- sub("acc", "acceleration", names(bothx))
+names(bothx) <- sub("mag", "magnitude", names(bothx))
+names(bothx) <- sub("^t", "time", names(bothx))
+names(bothx) <- sub("^f", "freq", names(bothx))
+## TODO here finish naming properly
+## TODO here
 
 
-# Properly combine test dataset files
+# Properly combine all dataset files into a single data frame
+allcombined <- cbind("subject"=bothsubject, "subjectclass"=bothset, "task"=bothy, bothx)
 
 
-
-# Load in training dataset files and properly combine
-trainx <- read.table("train/X_train.txt")
-trainy <-  read.table("train/y_train.txt")$V1
-trainsubject <- read.table("train/subject_train.txt")$V1
-
-
-# Merge test and training dataset
 
 
 
